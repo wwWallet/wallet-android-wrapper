6801f40b6181e75e2ccf06e5e231d2f593c5571d
Mario Bodemann <mario.bodemann@yubico.com>
2025-05-09 11:40:49 +0200

    polish: remove uneeded duplication and dependency providers

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,11 +13,9 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven("https://jitpack.io")
     }
 }
 
